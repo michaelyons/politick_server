c7de765662b422df22ee10da313b2f24266cc56f
Michael J. Lyons <mikelyons7@me.com>
2018-11-16 13:02:58 -0700

    Add dependcies for twitter

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const session = require('express-session');
+const passport = require('passport');
+const TwitterStrategy = require('passport-twitter').Strategy;
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -9,6 +16,8 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(helmet());
 
 app.get('/', (request, response) => {
   response.send('werks');
