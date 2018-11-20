588594d8147adcbbcf69e356e12ff1baa4f56f10
Michael J. Lyons <mikelyons7@me.com>
2018-11-20 14:15:37 -0700

    Add tweet login auth and post tweet to twitter route

--- routes/api/twitter.js
+++ routes/api/twitter.js
@@ -1,15 +1,59 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-// const Joi = require('joi');
+const Joi = require('joi');
 
-// var Twitter = require('twitter');
+var Twitter = require('twitter');
 
-// const User = require('../models/User');
+const User = require('../../models/User');
 
-//REQUEST TOKEN
-// router.get('/login', passport.authenticate('twitter'));
+//request token
+router.get('/login', passport.authenticate('twitter'));
 
-router.get('/test', (req, res) => res.json({ msg: 'twitter werks' }));
+//return token
+router.get(
+  '/return',
+  passport.authenticate('twitter', { session: false }),
+  async (request, response) => {
+    const { _id } = request.user;
+    return response.redirect(`https://informant-lobby.herokuapp.com/${_id}`);
+  }
+);
+
+//tweet post request
+router.post('/posttweet', async (request, response) => {
+  const user = await User.findById(request.body.id).select('twitterProvider');
+
+  const client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_KEY_SECRET,
+    access_token_key: user.twitterProvider.token,
+    access_token_secret: user.twitterProvider.tokenSecret
+  });
+
+  client
+    .post('/statuses/update', {
+      status: request.body.status
+    })
+    .then(function(tweet) {
+      console.log(tweet);
+    })
+    .catch(function(err) {
+      console.log(err.message);
+    });
+});
+
+const userValidate = user => {
+  const schema = {
+    name: Joi.string()
+      .min(2)
+      .required(),
+    twitterProvider: Joi.object().keys({
+      twitter_id: Joi.string()
+    }),
+    image: Joi.string()
+  };
+  return Joi.validate(user, schema);
+};
 
 module.exports = router;
