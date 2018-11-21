36e962406b9b817366b949ffbbb1df4bc46eabce
Michael J. Lyons <mikelyons7@me.com>
2018-11-21 11:07:21 -0700

    Edit return twitter url

--- routes/api/twitter.js
+++ routes/api/twitter.js
@@ -15,8 +15,8 @@ router.get(
   '/return',
   passport.authenticate('twitter', { session: false }),
   async (request, response) => {
-    const { _id } = request.user;
-    return response.redirect(`https://informant-lobby.herokuapp.com/${_id}`);
+    // const { _id } = request.user;
+    return response.redirect(`https://informant-lobby.herokuapp.com/`);
   }
 );
 
